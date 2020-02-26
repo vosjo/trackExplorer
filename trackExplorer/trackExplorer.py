7fc16229b643d6fd5893550e1bccc1df306171ce
Joris Vos <vos.joris@gmail.com>
2020-02-26 15:13:24 +0100

    read from actual mesa data, add x and y axis controls

--- trackExplorer/trackExplorer.py
+++ trackExplorer/trackExplorer.py
@@ -1,7 +1,7 @@
  
 #Load the packages
 import pandas as pd
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from bokeh.embed import components 
 from bokeh.models import HoverTool
 
@@ -10,13 +10,16 @@ from bokeh.plotting import figure
 #Connect the app
 app = Flask(__name__)
 
+#Get the data, from somewhere
+df = pd.read_csv('http://www.astro.physik.uni-potsdam.de/~jorisvos/ModelData/BesanconLargeNew1.csv')
+
 #Helper function
-def get_plot(df):
+def get_plot(df, xpar, ypar):
     #Make plot and customize
-    p = figure(x_axis_label='Sepal Length [cm]', 
-                y_axis_label='Sepal Width [cm]', title='Sepal width vs. length')
-    p.scatter(x='sepal_length', y='sepal_width', source=df )
-    #p.title.text_font_size = '16pt'
+    p = figure(x_axis_label=xpar, 
+                y_axis_label=ypar, title='MESA models: BesanconLargeNew1')
+    p.scatter(x=xpar, y=ypar, source=df )
+    p.title.text_font_size = '16pt'
     #p.add_tools(HoverTool()) #Need to configure tooltips for a good HoverTool
 
     #Return the plot
@@ -24,19 +27,21 @@ def get_plot(df):
 
 @app.route('/')
 def homepage():
-
-    #Get the data, from somewhere
-    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', 
-                     names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
-      
-    print (df)
+    
+    xpar = request.args.get("xpar")
+    if xpar is None:
+       xpar = df.columns[0]
+    ypar = request.args.get("ypar")
+    if ypar is None:
+       ypar = df.columns[1]
       
     #Setup plot    
-    p = get_plot(df)
+    p = get_plot(df, xpar, ypar)
     script, div = components(p)
 
     #Render the page
-    return render_template('home.html', script=script, div=div)    
+    return render_template('home.html', script=script, div=div, parameters=df.columns.values.tolist(),
+                           current_xpar=xpar, current_ypar=ypar)    
 
 if __name__ == '__main__':
     app.run(debug=True) #Set to false when deploying
